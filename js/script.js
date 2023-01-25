eb12e6c72806d64b0a4aa6845913ff3dc6639eec
Divyajyoti <divyajyoti182000@gmail.com>
2023-01-25 23:34:33 +0530

    All UI parts are completed and  working on game logic

--- js/script.js
+++ js/script.js
@@ -18,3 +18,32 @@ window.addEventListener('load', (e) => {
     e.preventDefault();
     rule.classList.remove('hidden');
 })
+
+//Mobile-Rules Card
+const mobile_btn_close = document.querySelector('.rules__mobile--closer');
+
+rule_btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    rule.classList.remove('hidden');
+});
+
+mobile_btn_close.addEventListener('click',e => {
+    e.preventDefault();
+    rule.classList.add('hidden');
+})
+
+///////////////////////////////////////////////////////////////
+const choice = ["rock", "paper", "scissor"];
+
+const p_paper = document.querySelector(".game__segment-1--paper");
+const p_scissor = document.querySelector(".game__segment-1--scissor");
+const p_rock = document.querySelector(".game__segment-2--rock");
+
+let player_choice = '';
+
+p_paper.addEventListener('click', (e) => {
+    e.preventDefault();
+    player_choice = choice[0];
+    
+})
+
