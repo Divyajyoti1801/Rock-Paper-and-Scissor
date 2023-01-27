263fce8b93d7f9ab903488762fe572cc381918e2
Divyajyoti <divyajyoti182000@gmail.com>
2023-01-27 19:24:45 +0530

    Project Completed

--- js/script.js
+++ js/script.js
@@ -49,6 +49,12 @@ const p_rock = document.querySelector(".game__segment-2--rock");
 //Player Choice
 let player_choice = "";
 
+let player_score = 0;
+
+//Player Score Text
+const player_score_text = document.querySelector(
+  ".header__banner--score-card--score-num"
+);
 
 //Player-Choice Code Segment to be hidden after selection has been made
 const game_segment_1 = document.querySelector(".game__segment-1");
@@ -58,7 +64,6 @@ const game_segment_2 = document.querySelector(".game__segment-2");
 const game_play_segment = document.querySelector(".game__play");
 const game_play_segment_1 = document.querySelector(".game__play--segment-1");
 const game_play_segment_2 = document.querySelector(".game__play--segment-2");
-
 
 //Result Output
 const game_play_result = document.querySelector(".game__play--segment-result");
@@ -71,11 +76,14 @@ const game_play_result_cta = document.querySelector(
   ".game__play--segment-result--cta"
 );
 
+player_score_text.innerHTML = 0;
+
 //Reset Button Functionality
-game_play_result_cta.addEventListener('click', (e) => {
-    e.preventDefault();
-    location.reload();
-})
+game_play_result_cta.addEventListener("click", (e) => {
+  e.preventDefault();
+  player_score_text.innerHTML = 0;
+  location.reload();
+});
 
 //Functionality and Activity
 
@@ -84,35 +92,40 @@ p_paper.addEventListener("click", (e) => {
   e.preventDefault();
   game_layout.style.backgroundImage = "none";
   player_choice = choice[1];
-  game_play_segment_1.insertAdjacentHTML('beforeend', player_pick_html(player_choice));
+  game_play_segment_1.insertAdjacentHTML(
+    "beforeend",
+    player_pick_html(player_choice)
+  );
   game_segment_1.style.display = "none";
   game_segment_2.style.display = "none";
   game_play_segment.classList.remove("hidden");
   const pick = housePick(choice);
   setTimeout(() => {
-      game_play_segment_2.insertAdjacentHTML('beforeend', house_pick_html(pick));
-      
-      if (player_choice === pick) {
-          game_play_segment_1.style.marginRight = "0";
-          game_play_result.classList.remove("hidden");
-          game_play_result_text.innerHTML = "Draw Match";
-      } else if (pick === 'scissors') {
-          game_play_segment_1.style.marginRight = "0";
-          game_play_result.classList.remove("hidden");
-          game_play_result_text.innerHTML = "You Loose";
-      } else if (pick === 'rock') {
-          game_play_segment_1.style.marginRight = "0";
-          game_play_result.classList.remove("hidden");
-          game_play_result_text.innerHTML = "You Win";
-      }
-   
+    game_play_segment_2.insertAdjacentHTML("beforeend", house_pick_html(pick));
+
+    if (player_choice === pick) {
+      game_play_segment_1.style.marginRight = "0";
+      game_play_result.classList.remove("hidden");
+      game_play_result_text.innerHTML = "Draw Match";
+    } else if (pick === "scissors") {
+      game_play_segment_1.style.marginRight = "0";
+      game_play_result.classList.remove("hidden");
+      game_play_result_text.innerHTML = "You Loose";
+      player_score = player_score - 1;
+    } else if (pick === "rock") {
+      game_play_segment_1.style.marginRight = "0";
+      game_play_result.classList.remove("hidden");
+      game_play_result_text.innerHTML = "You Win";
+      player_score += 1;
+    }
+    player_score_text.innerHTML = player_score;
   }, 4000);
 });
 
 //2. When Rock Clicked
 p_rock.addEventListener("click", (e) => {
-    e.preventDefault();
-    game_layout.style.backgroundImage = "none";
+  e.preventDefault();
+  game_layout.style.backgroundImage = "none";
   player_choice = choice[0];
   game_play_segment_1.insertAdjacentHTML(
     "beforeend",
@@ -133,19 +146,22 @@ p_rock.addEventListener("click", (e) => {
       game_play_segment_1.style.marginRight = "0";
       game_play_result.classList.remove("hidden");
       game_play_result_text.innerHTML = "You Win";
+      player_score += 1;
     } else if (pick === "paper") {
       game_play_segment_1.style.marginRight = "0";
       game_play_result.classList.remove("hidden");
       game_play_result_text.innerHTML = "You Loose";
+      player_score -= 1;
     }
+    player_score_text.innerHTML = player_score;
   }, 2000);
 });
 
 //3. When Scissors Clicked
 
 p_scissor.addEventListener("click", (e) => {
-    e.preventDefault();
-    game_layout.style.backgroundImage = "none";
+  e.preventDefault();
+  game_layout.style.backgroundImage = "none";
   player_choice = choice[2];
   game_play_segment_1.insertAdjacentHTML(
     "beforeend",
@@ -166,14 +182,16 @@ p_scissor.addEventListener("click", (e) => {
       game_play_segment_1.style.marginRight = "0";
       game_play_result.classList.remove("hidden");
       game_play_result_text.innerHTML = "You Win";
+      player_score += 1;
     } else if (pick === "rock") {
       game_play_segment_1.style.marginRight = "0";
       game_play_result.classList.remove("hidden");
       game_play_result_text.innerHTML = "You Loose";
+      player_score -= 1;
     }
+    player_score_text.innerHTML = player_score;
   }, 4000);
 });
-
 
 //Choosing [Rock,paper,Scissors] Randomly
 const housePick = (choice) => {
@@ -181,11 +199,6 @@ const housePick = (choice) => {
   const item = choice[random_pick];
   return item;
 };
-
-
-
-
-
 
 // Pick of House: Render
 const house_pick_html = (pick) => {
